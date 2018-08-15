383dd5b8e1d0adfef6ccc9c53fc40fa77e5f98f8
Jamie Burns <1443443+fantasticjamieburns@users.noreply.github.com>
2018-08-15 10:21:00 +0100

    Modify to better suit multiple modules

--- key-vault.tf
+++ key-vault.tf
@@ -1,13 +1,3 @@
-variable "tenant_id" {
-  type        = "string"
-  description = "The Tenant ID of the Azure Active Directory"
-}
-
-variable "jenkins_AAD_objectId" {
-  type        = "string"
-  description = "This is the ID of the Application you wish to give access to the Key Vault via the access policy"
-}
-
 module "ia-vault" {
   source                  = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
   name                    = "${var.product}-${var.env}"
@@ -16,7 +6,7 @@ module "ia-vault" {
   tenant_id               = "${var.tenant_id}"
   object_id               = "${var.jenkins_AAD_objectId}"
   resource_group_name     = "${azurerm_resource_group.rg.name}"
-  product_group_object_id = "b16db4e7-5cd6-409a-9f10-632089d91ff5"
+  product_group_object_id = "${var.ia_product_group_object_id}"
 }
 
 output "vaultName" {

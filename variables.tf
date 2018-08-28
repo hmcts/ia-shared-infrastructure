e47477918780039a6f75485de7daa3360e99597c
Jamie Burns <1443443+fantasticjamieburns@users.noreply.github.com>
2018-08-28 09:45:05 +0100

    Application Insights

--- variables.tf
+++ variables.tf
@@ -28,3 +28,16 @@ variable "ia_product_group_object_id" {
   type    = "string"
   default = "b16db4e7-5cd6-409a-9f10-632089d91ff5"
 }
+
+// as of now, UK South is unavailable for Application Insights
+variable "appinsights_location" {
+  type        = "string"
+  default     = "West Europe"
+  description = "Location for Application Insights"
+}
+
+variable "appinsights_application_type" {
+  type        = "string"
+  default     = "Web"
+  description = "Type of Application Insights (Web/Other)"
+}

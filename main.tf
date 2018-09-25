d5ecc12051a7e6fb03b6f2c35f1abb7453bc45cf
Jamie Burns <1443443+fantasticjamieburns@users.noreply.github.com>
2018-09-25 10:38:50 +0100

    If a preview infrastructure is built, allow it to be destroyed

--- main.tf
+++ main.tf
@@ -1,8 +1,11 @@
 locals {
+
+  destroy_me = "${var.env == "preview" || var.env == "spreview" ? "Yes" : var.destroy_me}"
+
   common_tags = {
     "environment"  = "${var.env}"
     "Team Name"    = "${var.team_name}"
     "Team Contact" = "${var.team_contact}"
-    "Destroy Me"   = "${var.destroy_me}"
+    "Destroy Me"   = "${local.destroy_me}"
   }
 }

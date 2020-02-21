1f91e1c2bc3a7c214272e2869440c50f260fe2c4
lee365 <leephilip365@yahoo.co.uk>
2020-02-21 10:54:52 +0000

    RIA-2647 EmailAlerts

---
+++ action-groups.tf
@@ -0,0 +1,18 @@
+// IAC Alerts Action Groups
+
+data "azurerm_key_vault_secret" "ia_support_email_secret" {
+  name      = "ia-support-email"
+  vault_uri = "${data.azurerm_key_vault.ia_key_vault.vault_uri}"
+}
+
+module "iac-action-group" {
+  source   = "git@github.com:hmcts/cnp-module-action-group"
+  location = "global"
+  env      = "${var.env}"
+
+  resourcegroup_name     = "${azurerm_resource_group.rg.name}"
+  action_group_name      = "ia-support"
+  short_name             = "ia-support"
+  email_receiver_name    = "IAC Support Mailing List"
+  email_receiver_address = "${data.azurerm_key_vault_secret.ia_support_email_secret.value}"
+}

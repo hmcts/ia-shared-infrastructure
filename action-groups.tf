3bc3995ac44a9d59ab6801e0808b913c76350dd1
lee365 <leephilip365@yahoo.co.uk>
2020-02-26 11:32:02 +0000

    emailId added directly

--- action-groups.tf
+++ action-groups.tf
@@ -1,10 +1,5 @@
 // IAC Alerts Action Groups
 
-data "azurerm_key_vault_secret" "ia_support_email" {
-  name      = "ia-support-email"
-  vault_uri = "${data.azurerm_key_vault.ia_key_vault.vault_uri}"
-}
-
 module "iac-action-group" {
   source   = "git@github.com:hmcts/cnp-module-action-group"
   location = "global"
@@ -14,5 +9,5 @@ module "iac-action-group" {
   action_group_name      = "ia-support"
   short_name             = "ia-support"
   email_receiver_name    = "IAC Support Mailing List"
-  email_receiver_address = "${data.azurerm_key_vault_secret.ia_support_email.value}"
+  email_receiver_address = "iac-support@hmcts.net"
 }

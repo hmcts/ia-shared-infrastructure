1836c34ad35712ae8fefad681eb51b89bae9ec7c
makowalski <marcin.kowalski.it.consulting@gmail.com>
2020-10-02 16:27:53 +0100

    Terraform upgrade 12.x (#23)

--- resource-group.tf
+++ resource-group.tf
@@ -1,8 +1,8 @@
 
 resource "azurerm_resource_group" "rg" {
   name     = "${var.product}-${var.env}"
-  location = "${var.location}"
-  tags     = "${merge(local.common_tags, map("lastUpdated", "${timestamp()}"))}"
+  location = var.location
+  tags     = merge(local.common_tags, map("lastUpdated", "${timestamp()}"))
 }
 
 output "resourceGroup" {

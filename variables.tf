b2473bc1bda419f7926879afd56dd65c138537c2
makowalski <marcin.kowalski.it.consulting@gmail.com>
2020-10-02 16:11:13 +0100

    Terraform upgrade 12.x remove deprecations

--- variables.tf
+++ variables.tf
@@ -1,62 +1,51 @@
 variable "product" {
-  type        = "string"
   default     = "ia"
   description = "The name of your application"
 }
 
 variable "env" {
-  type        = "string"
   description = "The deployment environment (sandbox, aat, prod etc..)"
 }
 
 variable "location" {
-  type    = "string"
   default = "UK South"
 }
 
 variable "tenant_id" {
-  type        = "string"
   description = "The Tenant ID of the Azure Active Directory"
 }
 
 variable "jenkins_AAD_objectId" {
-  type        = "string"
   description = "This is the ID of the Application you wish to give access to the Key Vault via the access policy"
 }
 
 variable "ia_product_group_object_id" {
-  type    = "string"
   default = "b16db4e7-5cd6-409a-9f10-632089d91ff5"
 }
 
 // as of now, UK South is unavailable for Application Insights
 variable "appinsights_location" {
-  type        = "string"
   default     = "West Europe"
   description = "Location for Application Insights"
 
 }
 
 variable "appinsights_application_type" {
-  type        = "string"
   default     = "web"
   description = "Type of Application Insights (Web/Other)"
 }
 
 variable "team_name" {
-  type        = "string"
   description = "Team name"
   default     = "Immigration"
 }
 
 variable "team_contact" {
-  type        = "string"
   description = "Team contact"
   default     = "#immigrationandasylum"
 }
 
 variable "destroy_me" {
-  type        = "string"
   description = "Here be dragons! In the future if this is set to Yes then automation will delete this resource on a schedule. Please set to No unless you know what you are doing"
   default     = "No"
 }

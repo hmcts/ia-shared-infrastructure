b4972540571fb15f9460f006b5cc13279bf0ed24
makowalski <marcin.kowalski.it.consulting@gmail.com>
2020-10-06 09:46:44 +0100

    migrate MI

--- key-vault.tf
+++ key-vault.tf
@@ -3,17 +3,18 @@ locals {
 }
 
 module "ia_key_vault" {
-  source                     = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
-  name                       = "${local.key_vault_name}"
-  location                   = "${var.location}"
-  resource_group_name        = "${azurerm_resource_group.rg.name}"
-  tenant_id                  = "${var.tenant_id}"
-  object_id                  = "${var.jenkins_AAD_objectId}"
-  product_group_object_id    = "${var.ia_product_group_object_id}"
-  env                        = "${var.env}"
-  product                    = "${var.product}"
-  common_tags                = "${local.common_tags}"
-  managed_identity_object_id = "${var.managed_identity_object_id}"
+  source                      = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
+  name                        = "${local.key_vault_name}"
+  location                    = "${var.location}"
+  resource_group_name         = "${azurerm_resource_group.rg.name}"
+  tenant_id                   = "${var.tenant_id}"
+  object_id                   = "${var.jenkins_AAD_objectId}"
+  product_group_object_id     = "${var.ia_product_group_object_id}"
+  env                         = "${var.env}"
+  product                     = "${var.product}"
+  common_tags                 = "${local.common_tags}"
+  managed_identity_object_ids = ["${var.managed_identity_object_id}"]
+  create_managed_identity     = true
 }
 
 output "vaultName" {

5b744b13ea13a2f90fa5976b1dd732174e2b30e2
rajeshthuraiyur <48379561+rajeshthuraiyur@users.noreply.github.com>
2022-08-08 15:25:31 +0100

    Update alerts.tf

--- alerts.tf
+++ alerts.tf
@@ -16,5 +16,5 @@ module "ia-exception-alert" {
   trigger_threshold = 0
   resourcegroup_name = "${azurerm_resource_group.rg.name}"
   enabled = "${var.env == "prod"}"
-  common_tags = "${local.common_tags}"
+  common_tags = "${var.common_tags}"
 }

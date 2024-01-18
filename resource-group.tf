2e22019ad954fc331091a853dcc9631cb7d2aeac
Chirag Pareek <111435448+cpareek@users.noreply.github.com>
2024-01-18 14:13:09 +0000

    DTSPO-15909 - migrated Application Insights (#39)
    * DTSPO-15909 - migrated Application Insights
    
    * adding renovate
    
    * updated azurerm
    
    * changed fucntion
    
    * udpated function
    
    * fix
    
    * added moved
    
    * fix
    
    * removed last moved block
    
    * Update action-groups.tf
    
    * Update action-groups.tf
    
    ---------
    
    Co-authored-by: Tyler-35 <115563258+Tyler-35@users.noreply.github.com>

--- resource-group.tf
+++ resource-group.tf
@@ -2,9 +2,9 @@
 resource "azurerm_resource_group" "rg" {
   name     = "${var.product}-${var.env}"
   location = var.location
-  tags     = merge(var.common_tags, map("lastUpdated", "${timestamp()}"))
+  tags     = merge(var.common_tags, tomap({ "lastUpdated" = "${timestamp()}" }))
 }
 
 output "resourceGroup" {
-  value = "${azurerm_resource_group.rg.name}"
+  value = azurerm_resource_group.rg.name
 }

587cf1c2e12225a0614d9c519fc113003603ebd6
Tyler-35 <115563258+Tyler-35@users.noreply.github.com>
2024-01-18 15:24:03 +0000

    Update alerts.tf

--- alerts.tf
+++ alerts.tf
@@ -8,11 +8,7 @@ module "ia-exception-alert" {
   alert_desc = "Triggers when an IAC exception is received in a 5 minute poll in ia-${var.env}."
   app_insights_query = "union traces, exceptions | where customDimensions[\"LoggingLevel\"] == \"ERROR\" | sort by timestamp desc"
   custom_email_subject = "Alert: Exception in ia-${var.env}"
-  frequency_in_minutes = 5
-  time_window_in_minutes = 5
-  severity_level = "2"
   action_group_name = "ia-support"
-  trigger_threshold_operator = "GreaterThan"
   trigger_threshold = 0
   resourcegroup_name = "${azurerm_resource_group.rg.name}"
   enabled = "${var.env == "prod"}"

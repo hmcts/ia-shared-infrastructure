663b4ce3a39746e97952dba0b62422ecf82de4af
58060 <62423932+hmcts-jenkins-d-to-i[bot]@users.noreply.github.com>
2025-08-21 09:56:24 +0000

    Updating Terraform Formatting

--- main.tf
+++ main.tf
@@ -4,16 +4,16 @@ provider "azurerm" {
 
 locals {
   common_tags = {
-    "environment"  = "${var.env}"
-    "Team Name"    = "${var.team_name}"
-    "Team Contact" = "${var.team_contact}"
-    "Destroy Me"   = "${var.destroy_me}"
-    "changeUrl"   = "${var.changeUrl}"
-    "managedBy"   = "${var.managedBy}"
-    "BuiltFrom"   = "${var.BuiltFrom}"
-    "contactSlackChannel"   = "${var.contactSlackChannel}"
-    "application"   = "${var.application}"
-    "businessArea"   = "${var.businessArea}"
+    "environment"         = "${var.env}"
+    "Team Name"           = "${var.team_name}"
+    "Team Contact"        = "${var.team_contact}"
+    "Destroy Me"          = "${var.destroy_me}"
+    "changeUrl"           = "${var.changeUrl}"
+    "managedBy"           = "${var.managedBy}"
+    "BuiltFrom"           = "${var.BuiltFrom}"
+    "contactSlackChannel" = "${var.contactSlackChannel}"
+    "application"         = "${var.application}"
+    "businessArea"        = "${var.businessArea}"
   }
 }
 
